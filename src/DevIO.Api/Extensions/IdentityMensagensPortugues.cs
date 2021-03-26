14adbbeec15493abdead650cc5dee896e438f6aa
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-26 14:04:14 -0300

    Traduzindo as msgs de erro do Identity

---
+++ src/DevIO.Api/Extensions/IdentityMensagensPortugues.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Extensions
+{
+    public class IdentityMensagensPortugues: IdentityErrorDescriber
+    {
+        public override IdentityError InvalidEmail(string email)
+        {
+            return new IdentityError { Code = nameof(InvalidEmail), Description = "Email inválido" };
+        }
+    }
+}
