1dadec03a1b69a3cd10ae2b0358045202f1fc762
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-26 10:06:42 -0300

    Adicionando as injeções de dependência referentes ao Identity

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.Api.Controllers
 {
+    [Route("api/conta")]
     public class AuthController : MainController
     {
-        public AuthController(INotificador notificador) : base(notificador)
+        private readonly SignInManager<IdentityUser> _signinManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotificador notificador,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notificador)
         {
+            _signinManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUserViewModel)
+        {
+            return CustomResponse(registerUserViewModel);
         }
     }
 }
