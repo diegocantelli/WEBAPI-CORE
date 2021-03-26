5583d3563acee9350d0f3e83e7b660ee1ef960b2
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-26 09:56:56 -0300

    Criação da controller de autenticação

---
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevIO.Business.Intefaces;
+
+namespace DevIO.Api.Controllers
+{
+    public class AuthController : MainController
+    {
+        public AuthController(INotificador notificador) : base(notificador)
+        {
+        }
+    }
+}
